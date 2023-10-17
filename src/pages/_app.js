28d027948c4f1477eba785284f1408acdcd45527
Rajkumar <rajkumarymb@gmail.com>
2023-10-17 15:13:36 +0530

    huntvasnewwebsite244

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,6 @@
 import { useEffect } from 'react';
 import Router from 'next/router';
 
-import 'assets/css/react-slick.css';
-import 'rc-drawer/assets/index.css';
 
 export default function CustomApp({ Component, pageProps }) {
  
